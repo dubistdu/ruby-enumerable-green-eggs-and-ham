0fca4b95c66f3799297de76e8511c555da3bff6b
Hyun Frantz <hyunfrantz@gmail.com>
2016-11-21 17:31:46 -0500

    adventure mode: test 6 done

--- green_eggs_and_ham.rb
+++ green_eggs_and_ham.rb
@@ -3,4 +3,32 @@ class GreenEggsAndHam
     @text = File.read('green_eggs_and_ham.txt')
     @words = @text.split(/[ .!?,\n]/).reject { |word| word.empty? }
   end
+
+  def word_count
+    @words.length
+  end
+
+  # more methods here!!!!
+  def sorted_unique_words
+    word_selected = @words.map {|word| word.downcase}.uniq.sort
+  end
+
+  def number_of_words_shorter_than(num)
+    @words.count {|word| word.length < num}
+  end
+
+  def longest_word
+    @words.max {|word| word.length}
+  end
+
+  #
+  def stanzas
+    @text.split("\n\n") # use every resources available!!!!
+  end
+
+  def lines
+    # string => array      => array   #.split will retun an array. It will split @text to an array
+    @text.split("\n").reject{ |line| line.empty? }
+  end
+
 end
--- test_green_eggs_and_ham.rb
+++ test_green_eggs_and_ham.rb
@@ -10,39 +10,32 @@ class GreenEggsAndHamTest < MiniTest::Test
   end
 
   def test_01_count_of_total_words
-    skip
     assert_equal 116, @book.word_count
   end
 
   def test_02_all_unique_words_sorted
-    skip
     expected = %w(a am and anywhere do eggs green ham here house i in
       like mouse not or sam sam-i-am that them there with would you)
     assert_equal expected, @book.sorted_unique_words
   end
 
   def test_03_count_of_words_less_than_four_letters
-    skip
     assert_equal 59, @book.number_of_words_shorter_than(4)
   end
 
   def test_04_longest_word
-    skip
     assert_match(/Sam-I-am/i, @book.longest_word)
   end
 
   def test_05_how_many_stanzas
-    skip
     assert_equal 6, @book.stanzas.count
   end
 
   def test_06_adventure_mode_line_count_should_not_include_blanks
-    skip
     assert_equal 19, @book.lines.count
   end
 
   def test_07_adventure_mode_all_unique_words_and_frequency
-    skip
     expected = {
         "i" => 15, "am" => 2, "sam" => 2, "that" => 3,
         "sam-i-am" => 6, "do" => 11, "not" => 13, "like" => 15,
